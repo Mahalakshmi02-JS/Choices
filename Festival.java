995291e68a9509e71144e86ce09251ded24a4124
Mahalakshmi02-JS <72906335+Mahalakshmi02-JS@users.noreply.github.com>
2020-10-20 22:13:16 +0530

    Add files via upload

---
+++ Festival.java
@@ -0,0 +1,21 @@
+public class Festival{
+public static void main(String[]js){
+	printMail();
+	printProgrammingLanguage("java");
+	printProgrammingLanguage("C");
+}
+	public static void printMail(){
+		System.out.println("maha@gmail.com");
+	}
+	public static void printProgrammingLanguage(String ProgrammingLanguage){
+		System.out.println("programmingLanguage:"+ProgrammingLanguage);
+
+	}
+	
+	
+}
+
+
+
+
+
