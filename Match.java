19c88d0979a74c6a277bf7f23ca41851d73a00c0
Mahalakshmi02-JS <72906335+Mahalakshmi02-JS@users.noreply.github.com>
2020-10-22 23:15:18 +0530

    Add files via upload

---
+++ Match.java
@@ -0,0 +1,23 @@
+public class Match{
+
+public static void main(String[] teams)
+{
+	
+	System.out.println("JVM invoked main");
+	//print refernce representation
+	System.out.println("teams"+ teams);
+	
+	int sizeOfTeams=teams.length;
+	System.out.println("sizeOfTeams " +sizeOfTeams);
+	System.out.println("Team at index 0 "+teams[0]);
+	System.out.println("Team at index 1 "+teams[1]);
+		System.out.println("Team at index 2 "+teams[2]);
+	System.out.println("Team at index 3 "+teams[3]);
+		System.out.println("Team at index 1 "+teams[1]);
+
+
+	System.out.println("exit  from main method");
+}
+
+
+}
