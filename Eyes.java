0f48f0bcad5969c9d4294ec487404eada8c800e0
Mahalakshmi02-JS <72906335+Mahalakshmi02-JS@users.noreply.github.com>
2020-10-23 21:27:53 +0530

    Add files via upload

---
+++ Eyes.java
@@ -0,0 +1,11 @@
+public class Eyes{
+public static void main(String[]nest)
+{
+System.out.println("Human body contains: ");
+Paper.Parts(nest[0]);
+Paper.Parts(nest[1]);
+Paper.Parts(nest[2]);
+
+
+}
+}
