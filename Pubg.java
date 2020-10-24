7d28345f3249265f937ed9ba4a1d19dac04dd19c
Mahalakshmi02-JS <72906335+Mahalakshmi02-JS@users.noreply.github.com>
2020-10-24 07:58:15 +0530

    Add files via upload

---
+++ Pubg.java
@@ -0,0 +1,9 @@
+public class Pubg{
+public static void supply()
+{
+System.out.println("Invokes supply from pub-g class");
+}
+public static void enemiesAhead(){
+System.out.println("Invoked enemies from pub-g class");
+}
+}
